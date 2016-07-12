68ff1169bad455a53467a14f605422f8c31eac8f
Alexis Kalyvitis <alexkalyvitis@Alexiss-MacBook-Pro.local>
2016-07-12 23:07:37 +0200

    fixing routing

--- web/index.php
+++ web/index.php
@@ -2,9 +2,19 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+/**
+ * See: http://silex.sensiolabs.org/doc/master/web_servers.html#php
+ */
+$filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
+
 use Varvara\Controllers\ProductController;
 
 $app = new Silex\Application();
+
+$app['debug'] = true;
 
 $app->get('/api/products', function () use ($app) {
   return $app->json(ProductController::getProducts());
@@ -22,8 +32,9 @@ $app->put('/api/carts/{id}', function ($id) use ($app) {
   return 'Cart is updated!!!';
 });
 
-$app->get('/', function () use ($app) {
-	return $app->sendFile('static/index.html');
-});
+$app->get('/{path}', function ($path) use ($app) {
+	return $app->sendFile('index.html');
+})
+->assert('path', '.*');
 
 $app->run();
