5c220ffddeb68dac0f0df03286c1268ee767f4db
Alexis Kalyvitis <alexkalyvitis@Alexiss-MacBook-Pro.local>
2016-07-14 22:03:11 +0200

    Product controller is using the product model

--- src/Varvara/Controllers/ProductController.php
+++ src/Varvara/Controllers/ProductController.php
@@ -2,31 +2,37 @@
 
 namespace Varvara\Controllers;
 
+use Varvara\Models\Product;
+
 class ProductController
 {
 	public static function getProducts()
-	{
+	{		
+		$productA = new Product();
+		$productA->setId(1);
+		$productA->setDescription('Product A');
+		$productA->setPrice(3.4);
+
+		$productB = new Product();
+		$productB->setId(2);
+		$productB->setDescription('Product B');
+		$productB->setPrice(2.6);
+
+		$productC = new Product();
+		$productC->setId(3);
+		$productC->setDescription('Product C');
+		$productC->setPrice(9.4);
+
+		$productD = new Product();
+		$productD->setId(4);
+		$productD->setDescription('Product D');
+		$productD->setPrice(1.1);
+		
 		return [
-			[
-				'id' => 1,
-				'description' => 'Product A',
-				'price' => '3.4'
-			],
-			[
-				'id' => 2,
-				'description' => 'Product B',
-				'price' => '1.3'
-			],
-			[
-				'id' => 3,
-				'description' => 'Product C',
-				'price' => '9.4'
-			],
-			[
-				'id' => 4,
-				'description' => 'Product D',
-				'price' => '2.3'
-			]
+			$productA,
+			$productB,
+			$productC,
+			$productD
 		];
 	}
 }
